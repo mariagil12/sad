54d9cc73b23012dc2fa72cc399e1805fc05d534d
mariagil12 <71631529+mariagil12@users.noreply.github.com>
2021-03-02 18:35:16 +0100

    editableBufferedReader.java

---
+++ editableBufferedReader.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class editableBufferedReader extends BufferedReader {
+	protected void setRaw() {
+		try{
+			String[] cmd = {"/bin/sh","-c", "stty -echo raw </dev/tty"};
+			Runtime.getRuntime().exec(cmd);
+		} catch ( IOException e) {
+			e.printStackTrace();;
+		}
+	}
+
+	protected void unsetRaw() {
+		try{
+			String[] cmd = {"/bin/sh","-c", "stty -raw echo </dev/tty"};
+			Runtime.getRuntime().exec(cmd);
+		} catch ( IOException e) {
+			e.printStackTrace();;
+		}
+	}
