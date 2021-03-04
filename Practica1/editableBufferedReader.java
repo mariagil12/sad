524724e6a34c7a9acceac463180b53f40c0fb503
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2021-03-04 09:11:32 +0100

    e -> E

--- Practica1/editableBufferedReader.java
+++ Practica1/editableBufferedReader.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class editableBufferedReader extends BufferedReader {
+public class EditableBufferedReader extends BufferedReader {
 	protected void setRaw() {
 		try{
 			String[] cmd = {"/bin/sh","-c", "stty -echo raw </dev/tty"};
