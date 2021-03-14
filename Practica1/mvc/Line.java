5cdfdf40b73b56f776ae4eaf40ad75c7c181400c
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2021-03-14 11:45:57 +0100

    Update Line.java

--- Practica1/mvc/Line.java
+++ Practica1/mvc/Line.java
@@ -1,5 +1,40 @@
 package mvc;
 
-public class Line {
+import java.util.*;
 
-}
+//Model
+@SuppressWarnings("deprecation")
+public class Line extends Observable{
+	
+	ArrayList<Integer> buf;
+	private int pos;
+	private int len;
+	private boolean ins;
+	private String[] action;
+	private StringBuilder str;
+	
+	public Line() {
+		this.buf=new ArrayList<>();
+		this.str=new StringBuilder();
+		this.pos=0;
+		this.len=0;
+		this.ins=false;
+		this.action=new String[2];
+	}
+	
+	public StringBuilder getLine() {
+		return this.str;
+	}
+	
+	public int getPos() {
+		return this.pos;
+	}
+	
+	public int getLength() {
+		return this.str.length();
+	}
+	
+	public Boolean getInsert() {
+		return this.ins;
+	}
+	
