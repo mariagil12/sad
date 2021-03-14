74614bfac58e9e8764924e048e133ed40ff78acd
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2021-03-14 10:52:52 +0100

    Update EditableBufferedReader.java

--- Practica1/mvc/EditableBufferedReader.java
+++ Practica1/mvc/EditableBufferedReader.java
@@ -3,13 +3,17 @@ package mvc;
 import java.io.*;
 
 
+//Controller
 public class EditableBufferedReader extends BufferedReader{
 	
 	private Line linea;
-	
+	private Console consola;
+		
 	public EditableBufferedReader(Reader in) {
 		super(in);
 		this.linea=new Line();
+		this.consola=new Console();
+		this.linea.addObserver(this.consola); //Me da error en addObserver()
 	}
 	
 }
