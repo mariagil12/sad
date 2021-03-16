b29611cffa738933ee030f282ad22139c1f4cbfa
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2021-03-16 16:06:28 +0100

    Update EditableBufferedReader.java

--- Practica1/mvc/EditableBufferedReader.java
+++ Practica1/mvc/EditableBufferedReader.java
@@ -2,18 +2,19 @@ package mvc;
 
 import java.io.*;
 
-
 //Controller
 public class EditableBufferedReader extends BufferedReader{
 	
 	private Line linea;
 	private Console consola;
 		
+	@SuppressWarnings("deprecation")
 	public EditableBufferedReader(Reader in) {
 		super(in);
 		this.linea=new Line();
 		this.consola=new Console();
-		this.linea.addObserver(this.consola); //Me da error en addObserver()
+		this.linea.addObserver(this.consola);
 	}
 	
 }
+
