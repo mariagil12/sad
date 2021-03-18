ac0061ebf2e0e4368f17613a3c13496c45fb12ac
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2021-03-18 08:46:30 +0100

    Update Console.java

--- Practica1/mvc/Console.java
+++ Practica1/mvc/Console.java
@@ -2,15 +2,14 @@ package mvc;
 
 import java.util.Observer;
 import java.util.Observable;
-
 //View
 @SuppressWarnings("deprecation")
 public class Console implements Observer {
 	@Override
 	public void update(Observable obs, Object obj) {
-		String[] action = (String []) obj;
-		if(action[0]=="true") {
-			System.out.println(action[1]);
+		String[] notify = (String []) obj;
+		if(notify[0]=="true") {
+			System.out.println(notify[1]);
 		}else {
 			System.out.println("Invalid input");
 		}
