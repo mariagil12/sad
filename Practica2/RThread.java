dfaf44c978db3393ab7b7cc6dff96cb63bd5bb75
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2021-03-23 17:57:35 +0100

    Add files via upload

---
+++ Practica2/RThread.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+
+public class RThread extends Thread{
+	private BufferedReader buf;
+	private MySocket client;
+	private Socket soc;
+	
+	public RThread (Socket s, MySocket ms) {
+		this.client=ms;
+		this.soc=s;
+		try {
+			InputStream in = soc.getInputStream();
+			buf=new BufferedReader(new InputStreamReader(in));
+		}catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while(true) {
+			try {
+				String read=buf.readLine();
+				System.out.println(read);
+				if(client.getUser()!=null) {
+					System.out.println("From: "+client.getUser()+"\n");
+				}
+			}catch(IOException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
