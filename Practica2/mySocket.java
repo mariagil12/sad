9a62397bd6dd799d4fc54e5a45d24cc419e9796f
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2021-03-23 17:48:05 +0100

    Update mySocket.java

--- Practica2/mySocket.java
+++ Practica2/mySocket.java
@@ -1,1 +1,46 @@
+import java.net.*;
+import java.io.*;
 
+public class MySocket {
+	private int port;
+	private String user;
+	private String host;
+	
+	public MySocket(String host, int port) {
+		this.host=host;
+		this.port=port;
+	}
+	
+	public String getUser() {
+		return this.user;
+	}
+	
+	public void setUser(String u) {
+		this.user=u;
+	}
+	
+	public void execute() {
+		try {
+			Socket soc=new Socket(this.host, this.port);
+			System.out.println("Successfully connected to server");
+			new RThread(soc,this).start();
+			new WThread(soc,this).start();
+		} catch(UnknownHostException e) {
+			System.out.println(e.getMessage());
+		} catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		if(args.length<2) {
+			System.out.println("Syntax error. Syntax should be: java MySocket <hostName> <portNumber>");
+			System.exit(0);
+		}
+		String host=args[0];
+		int port= Integer.parseInt(args[1]);
+		
+		MySocket client = new MySocket(host,port);
+		client.execute();
+	}
+}
