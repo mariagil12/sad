27d902fec7f51909ad1ae2a83630262a1bb35052
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2021-04-06 12:09:45 +0200

    Update MyServerSocket.java

--- Practica2/MyServerSocket.java
+++ Practica2/MyServerSocket.java
@@ -1,9 +1,7 @@
-package sad_practica2;
-
 import java.io.*;
 import java.util.*;
-import java.net.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.net.*;
 
 public class MyServerSocket {
 	private int port;
@@ -15,12 +13,11 @@ public class MyServerSocket {
 	}
 	
 	public void execute() {
-		try {
-			ServerSocket serverSocket = new ServerSocket(port);
-			System.out.println("Successfully connected to "+port);
+		try (ServerSocket serverSocket = new ServerSocket(port)){
+			System.out.println("Server listening to port"+port);
 			while (true) {
 				Socket socket = serverSocket.accept();
-				System.out.println("New connection!");
+				System.out.println("Client successfully connected");
 				Server newUsr = new Server (socket, this);
 				newUsr.start();
 			}
@@ -37,36 +34,37 @@ public class MyServerSocket {
 		server.execute();
 	}
 	
-	// Enviar missatge a bradcast
+	// Enviar missatge a broadcast
 	void broadcast(String message, String excludeUser) {
-		for(String key : this.clients.keySet() ) {
+		for(String key : clients.keySet() ) {
 			if (!key.equals(excludeUser)) {
-				this.clients.get(key).sendMessage(message);
+				clients.get(key).sendMessage(message);
 			}
 		}
 	}
 	
   // Afegir clients: guarda username i el thread server 
 	void addUserName(String userName, Server server) {
-		this.clients.put(userName,server);
+		clients.put(userName,server);
 	}
 	
   // Eliminar client: eliminar username i threads
   // S'envia missatge als altres users
 	void removeUser(String userName, Server server) {
-		this.clients.remove(userName,server);
-		System.out.println("The user " + userName + " has quitted.");
+		clients.remove(userName,server);
+		System.out.println("The user " + userName + " quitted.");
 	}
 	
 	Set<String> getUserNames() {
 		Set<String> userNames = new HashSet<>();
-		for (String key : this.clients.keySet()) {
+		for (String key : clients.keySet()) {
 			userNames.add(key);
 		}
 		return userNames;
 	}
 	
-	boolean hasUser() {
-		return !this.clients.isEmpty();
+	// Retorna true si hi ha més usuaris connectats
+	boolean moreUsers() {
+		return !clients.isEmpty();
 	}
 }
