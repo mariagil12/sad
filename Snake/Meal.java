5c12588c983696a90946a03d4eb77595f3f64596
mariagil12 <71631529+mariagil12@users.noreply.github.com>
2021-04-20 18:54:28 +0200

    Create Meal.java

---
+++ Snake/Meal.java
@@ -0,0 +1,34 @@
+package sad_Snake;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.Random;
+
+public class Meal {
+	private int maxX = 800;
+	private int maxY = 600;
+	private Random random;
+	private Point food;
+	
+	public Meal() {
+		this.random = new Random();
+		this.food = new Point();
+	}
+	
+	public void newFood() {
+		food.x = random.nextInt(maxX);
+		food.y = random.nextInt(maxY);
+	}
+	
+	public void drawFood(Graphics g) {
+		g.create();
+		g.setColor(Color.RED);
+		g.drawOval(food.x, food.y, 10, 10);
+		g.fillOval(food.x, food.y, 10, 10);
+	}
+	
+	public Point getFood() {
+		return food;
+	}
+}
