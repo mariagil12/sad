782969e677d26a2d36780b4b1bcbbfd8fb0d1a5a
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2021-05-04 17:07:36 +0200

    Update Meal.java

--- Snake/Meal.java
+++ Snake/Meal.java
@@ -4,8 +4,8 @@ import java.awt.Point;
 import java.util.Random;
 
 public class Meal {
-	private int maxX = 790; //para que no se cree en el borde
-	private int maxY = 562; //restamos la longitud de la barra de la frame para que no se cree ahi
+	private int maxX = 775; //para que no se cree en el borde
+	private int maxY = 542; //restamos la longitud de la barra de la frame para que no se cree ahi
 	private Random random;
 	private Point food;
 	
@@ -15,18 +15,18 @@ public class Meal {
 	}
 	
 	public void newFood() {
-		food.x = 5+random.nextInt(maxX);
-		food.y = 33 + random.nextInt(maxY);
+		food.x = 10 + random.nextInt(maxX);
+		food.y = 38 + random.nextInt(maxY);
 	}
 	
 	public void drawFood(Graphics g) {
 		g.create();
 		g.setColor(Color.RED);
-		// g.drawOval(food.x, food.y, 10, 10);
 		g.fillOval(food.x, food.y, 10, 10);
 	}
 	
 	public Point getFood() {
 		return food;
 	}
+}
 }
