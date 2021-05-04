892cc8e26fad883a2a264d2477e0b8167e1534a0
mariagil12 <71631529+mariagil12@users.noreply.github.com>
2021-05-04 22:39:52 +0200

    Update Play.java

--- Snake/Play.java
+++ Snake/Play.java
@@ -13,6 +13,14 @@ public class Play extends JFrame implements KeyListener {
 	private int score;
 	private long goal;
 	private int timePassed;
+	private int i=0;
+	private int j=0;
+	private int k=0;
+	private boolean presed=false;
+	private boolean menu=true;
+	private boolean colors=false;
+	private boolean play=false;
+	private int time = 10;
 			
 	
 	private int minX = 0;
@@ -35,6 +43,43 @@ public class Play extends JFrame implements KeyListener {
 		initializeObjects();
 		
 		while(true) {
+			while(menu==true) {
+				showMenu(i);
+				sleepMenu();
+				if (presed==true) {
+					menu=false;
+				}
+			}
+			switch(i) {
+			case 0:
+				play=true;
+				playSnake();
+				play=false;
+				break;
+			case 1:
+				// no funciona perque al apretarr enter colors no es cambia a false
+				initializeObjects();
+				colors=true;
+				while(colors=true) {
+					System.out.println(colors);
+					showColors(j,k);
+					sleepMenu();
+				}
+				System.out.println(colors);
+				showMenu(i);
+				break;
+			case 2:
+				// exit
+				System.exit(0);
+				break;
+			}
+		}
+	}
+	
+	private void playSnake() {
+		initializeObjects();
+		
+		while(true) {
 			play();
 			sleep();
 		}
@@ -51,6 +96,144 @@ public class Play extends JFrame implements KeyListener {
 		snake.moveSnake();
 		checkCollition();
 		showDraw();
+	}
+	private void showColors(int j, int k) {
+		BufferStrategy bf = this.getBufferStrategy();
+		Graphics g = null;
+		try {
+			g = bf.getDrawGraphics();			
+			g.setColor(Color.PINK);
+			g.fillRect(minX, minY, windowWidth, windowHeight);
+			g.setColor(Color.BLACK);
+			g.drawRect(minX, minY, windowWidth, windowHeight);
+			
+			g.setColor(Color.GRAY);
+			g.setFont(new Font("Arial", Font.BOLD, 66));
+			g.drawString("COLORS", 280, 150);
+			g.setColor(Color.BLACK);
+			g.fillRect(125, 200, 100, 100);
+			g.setColor(Color.BLUE);
+			g.fillRect(275, 200, 100, 100);
+			g.setColor(Color.CYAN);
+			g.fillRect(425, 200, 100, 100);
+			g.setColor(Color.DARK_GRAY);
+			g.fillRect(575, 200, 100, 100);
+			g.setColor(Color.GRAY);
+			g.fillRect(125, 330, 100, 100);
+			g.setColor(Color.GREEN);
+			g.fillRect(275, 330, 100, 100);
+			g.setColor(Color.MAGENTA);
+			g.fillRect(425, 330, 100, 100);
+			g.setColor(Color.ORANGE);
+			g.fillRect(575, 330, 100, 100);
+			g.setColor(Color.PINK);
+			g.fillRect(125, 460, 100, 100);
+			g.setColor(Color.RED);
+			g.fillRect(275, 460, 100, 100);
+			g.setColor(Color.WHITE);
+			g.fillRect(425, 460, 100, 100);
+			g.setColor(Color.YELLOW);
+			g.fillRect(575, 460, 100, 100);
+			g.setColor(Color.BLACK);
+			switch(j) {
+			case 0:
+				switch(k) {
+				case 0:
+					g.drawRect(122, 197, 106, 106);
+					break;
+				case 1:
+					g.drawRect(122, 327, 106, 106);
+					break;
+				case 2:
+					g.drawRect(122, 457, 106, 106);
+					break;
+				}
+				break;
+			case 1:
+				switch(k) {
+				case 0:
+					g.drawRect(272, 197, 106, 106);
+					break;
+				case 1:
+					g.drawRect(272, 327, 106, 106);
+					break;
+				case 2:
+					g.drawRect(272, 457, 106, 106);
+					break;
+				}
+				break;
+			case 2:
+				switch(k) {
+				case 0:
+					g.drawRect(422, 197, 106, 106);
+					break;
+				case 1:
+					g.drawRect(422, 327, 106, 106);
+					break;
+				case 2:
+					g.drawRect(422, 457, 106, 106);
+					break;
+				}
+				break;
+			case 3:
+				switch(k) {
+				case 0:
+					g.drawRect(572, 197, 106, 106);
+					break;
+				case 1:
+					g.drawRect(572, 327, 106, 106);
+					break;
+				case 2:
+					g.drawRect(572, 457, 106, 106);
+					break;
+				}
+				break;
+			}
+		} finally {
+			g.dispose();
+		}
+		bf.show();
+		Toolkit.getDefaultToolkit().sync();
+	}
+	
+	private void showMenu(int i) {
+		BufferStrategy bf = this.getBufferStrategy();
+		Graphics g = null;
+		try {
+			g = bf.getDrawGraphics();			
+			g.setColor(Color.PINK);
+			g.fillRect(minX, minY, windowWidth, windowHeight);
+			g.setColor(Color.BLACK);
+			g.drawRect(minX, minY, windowWidth, windowHeight);
+			
+			g.setColor(Color.GRAY);
+			g.setFont(new Font("Arial", Font.BOLD, 66));
+			g.drawString("SNAKE", 280, 150);
+			g.setColor(Color.DARK_GRAY);
+			g.setFont(new Font("Arial", Font.BOLD, 46));
+			g.drawString("1. Play", 200, 250);
+			g.drawString("2. Change snake color", 200, 350);
+			g.drawString("3. Exit", 200, 450);
+			switch(i) {
+			case 0:
+				g.setColor(Color.RED);
+				g.fillOval(170, 230, 15, 15);
+				break;
+			case 1:
+				g.setColor(Color.RED);
+				g.fillOval(170, 330, 15, 15);
+				break;
+			case 2:
+				g.setColor(Color.RED);
+				g.fillOval(170, 430, 15, 15);
+				break;
+			}
+			
+		} finally {
+			g.dispose();
+		}
+		bf.show();
+		Toolkit.getDefaultToolkit().sync();
 	}
 	
 	private void showDraw() {
@@ -121,25 +304,130 @@ public class Play extends JFrame implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		switch(key) {
-			case KeyEvent.VK_UP:
+		case KeyEvent.VK_UP:
+			if (menu==true) {
+				i=i+2;
+				i=i%3;
+				System.out.println(i);
+			} else if(play==true){
 				snake.direction("UP");
-				break;
-			case KeyEvent.VK_DOWN:
+			} else if(colors==true) {
+				k=k+2;
+				k=k%3;
+			}
+			break;
+		case KeyEvent.VK_DOWN:
+			//snake.direction("DOWN");
+			if (menu==true) {
+				i ++;
+				i=i%3;
+				System.out.println(i);
+			} else if(colors==true) {
+				k++;
+				k=k%3;
+			} else if (play==true){
 				snake.direction("DOWN");
-				break;
-			case KeyEvent.VK_RIGHT:
+			}
+			break;
+		case KeyEvent.VK_RIGHT:
+			if(colors==true) {
+				j++;
+				j=j%4;
+			} else if(play==true) {
 				snake.direction("RIGTH");
-				break;
-			case KeyEvent.VK_LEFT:
+			}
+			break;
+		case KeyEvent.VK_LEFT:
+			if(colors==true) {
+				j=j+3;
+				j=j%4;
+			} else if(play==true) {
 				snake.direction("LEFT");
-				break;
-			case KeyEvent.VK_E:
-				System.exit(0);
-			case KeyEvent.VK_SPACE:
-				snake.pause = !snake.pause;
-				break;
-		}
+			}
+			break;
+		case KeyEvent.VK_E:
+			System.exit(0);
+			break;
+		case KeyEvent.VK_SPACE:
+			snake.pause = !snake.pause;
+			break;
+		case KeyEvent.VK_ENTER:
+			System.out.println(colors);
+			System.out.println('A');
+			if (colors==true) {
+				System.out.println(colors);
+				System.out.println('B');
+				colors=!colors;
+				System.out.println(colors);
+				switch(j) {
+				case 0:
+					switch(k) {
+					case 0:
+						snake.color=0; // black
+						break;
+					case 1:
+						snake.color=1; //gris
+						break;
+					case 2:
+						snake.color=2; // rosa
+						break;
+					}
+					break;
+				case 1:
+					switch(k) {
+					case 0:
+						snake.color=3;
+						//blau
+						break;
+					case 1:
+						snake.color=4;
+						//verd
+						break;
+					case 2:
+						snake.color=5;
+						//vermell
+						break;
+					}
+					break;
+				case 2:
+					switch(k) {
+					case 0:
+						snake.color=6;
+						//cyan
+						break;
+					case 1:
+						snake.color=7;
+						//magenta
+						break;
+					case 2:
+						snake.color=8;
+						//blanc
+						break;
+					}
+					break;
+				case 3:
+					switch(k) {
+					case 0:
+						snake.color=9;
+						//grisfosc
+						break;
+					case 1:
+						snake.color=10;
+						//tronja
+						break;
+					case 2:
+						snake.color=11;
+						//groc
+						break;
+					}
+					break;
+				}
+				menu=true;
+				return;
+				} else if(menu==true) {
+					presed=true;
+					menu=false;
+				}
 	}
 		
 	@Override
